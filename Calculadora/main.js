a9eab1cdceeb0c131d357521691fb06e3f63475b
rmillanv <rmillan6@gmail.com>
2024-10-08 07:21:16 -0500

    subiendo proyectos

--- Calculadora/main.js
+++ Calculadora/main.js
@@ -1,9 +1,38 @@
 const botones = document.querySelectorAll(".btn")
-const pantalla = document.querySelector(".pantalla")
+const pantalla = document.querySelector("#pantalla")
 
+console.log(botones)
 botones.forEach(boton =>{
     boton.addEventListener("click",()=>{
         const botonApretado = boton.textContent
-        pantalla.textContent = botonApretado
+        if(botonApretado == "AC"){
+            pantalla.textContent = "0"
+        }
+        else if(botonApretado == "Del"){
+            if(pantalla.textContent.length ==1){
+                pantalla.textContent ="0"
+            }else{
+                pantalla.textContent = pantalla.textContent.slice(0,1)
+            }
+        }else if(botonApretado == "="){
+            try{
+                pantalla.textContent = eval(pantalla.textContent)
+            }catch (error) {
+                pantalla.textContent = "Error!"
+            }
+        }else if(botonApretado == "%"){
+            try{
+                pantalla.textContent = eval(pantalla.textContent)*0.01
+            }catch(error){
+                pantalla.textContent = "Error!"
+            }
+        }
+        else{
+            if(pantalla.textContent == "0" || pantalla.textContent == "Error!"){
+                pantalla.textContent = botonApretado
+            }else{
+                pantalla.textContent = pantalla.textContent + botonApretado
+            }
+        }
     })
 })
